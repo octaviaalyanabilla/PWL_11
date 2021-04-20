b1ce05cbf627ec79e756c5dd6e16583d632b06ba
ACER\Z1402 <octaviaalya01@gmail.com>
2021-04-20 13:53:42 +0700

    Mengisi ApiRequest

--- app/Http/Requests/ApiRequest.php
+++ app/Http/Requests/ApiRequest.php
@@ -4,27 +4,31 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ApiRequest extends FormRequest
+abstract class ApiRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
-
+    use ApiResponse;
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
-    public function rules()
+    abstract public function rules();
+
+    public function failedValidation(Validator $validator)
     {
-        return [
-            //
-        ];
+        throw new HttpResponseException($this->apiError(
+           $validator->errors(),
+           Response::HTTP_UNPROCESSABLE_ENTITY, 
+        ));
+    }
+
+   
+    public function failedAuthorization()
+    {
+        throw new HttpResponseException($this->apiError(
+            null,
+            Response::HTTP_UNAUTHORIZED
+        ));
+    
     }
 }
